1760f321a351f2e4adbd271518d967dbdc59d2ae
mcollins1010 <61592185+mcollins1010@users.noreply.github.com>
2024-03-20 23:25:40 -0400

    change keypath

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetris-bucket001" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "eks/terraform.tfstate"
     region = "us-east-2"
   }
 }
